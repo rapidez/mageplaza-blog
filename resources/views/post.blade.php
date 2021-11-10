4bf560a698e82950a47487aff317b14218a808a8
Roy Duineveld <royduineveld@gmail.com>
2021-11-10 09:49:54 +0100

    Use HTML5 semantic tag

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,9 @@
     <div class="container mx-auto mb-5">
         <img src="{{ config('rapidez.media_url') }}/mageplaza/blog/post/{{ $post->image }}" alt="">
 
-        <div class="my-5 prose prose-green max-w-none">
+        <article class="my-5 prose prose-green max-w-none">
             {!! $post->post_content !!}
-        </div>
+        </article>
 
         <div class="text-sm text-gray-400">
             @lang('Published at :date', [
