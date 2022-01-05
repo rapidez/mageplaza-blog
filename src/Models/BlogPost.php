ab97e2261d7361532f72f6248655d324eeeba2a5
Bob <bob@justbetter.nl>
2022-01-05 14:09:26 +0100

    Added publish date to query

--- src/Models/BlogPost.php
+++ src/Models/BlogPost.php
@@ -2,6 +2,7 @@
 
 namespace Rapidez\MageplazaBlog\Models;
 
+use Carbon\Carbon;
 use Rapidez\Core\Models\Model;
 use Rapidez\Core\Models\Scopes\IsActiveScope;
 
@@ -16,5 +17,8 @@ class BlogPost extends Model
     protected static function booted()
     {
         static::addGlobalScope(new IsActiveScope('enabled'));
+        static::addGlobalScope('only_published_posts', function ($builder) {
+            $builder->where('publish_date', '<=', Carbon::now());
+        });
     }
 }
